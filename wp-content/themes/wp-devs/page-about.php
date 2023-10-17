4be27b5399f2f7e284871485f720ca66efd9d322
mofazzal-hossain <mosharofmd74@gmail.com>
2023-10-17 17:21:58 +0600

    post query

--- wp-content/themes/wp-devs/page-about.php
+++ wp-content/themes/wp-devs/page-about.php
@@ -1,6 +1,41 @@
 <?php get_header();?>
 
+<div class="container">
+    <div class="latest-news">
+        <h3>Lastest News</h3>
+        <?php 
+            $arg =[
+                'post_type' => 'post',
+                'post_per_page'=>5,
+                'category__not_in'=> array(1),
+            ];
+                
+            $postList = new WP_Query($arg);
 
-<h3>this is about page</h3>
+            if($postList->have_posts()) : 
+                while($postList->have_posts()) : $postList->the_post();
+                  ?>
+                    <article> 
+                        <?php the_post_thumbnail('large');?>
+                        <h3><?php echo the_title(); ?></h3>
+                        <div class="meta-info">
+                            <p>
+                                by <span><?php the_author_posts_link(); ?></span>
+                                categories <span><?php the_category(', ') ;?></span>
+                                tags <span><?php the_tags() ;?></span>
+                            </p>
+                            <p><?php the_date() ;?></p>
+                        </div>
+                        <p><?php the_content(); ?></p>
+                    </article>
+                  <?php
+                endwhile;
+                wp_reset_postdata();
+            endif;
+
+            
+        ?>
+    </div>
+</div>
 
 <?php get_footer();?>
